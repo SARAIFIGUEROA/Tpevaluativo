5fa1f5597ece1068d25689f3e9797aa83e20d405
SARAIFIGUEROA <Saraifigueroa012@gmail.com>
2024-06-26 11:11:10 -0300

    tp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IniciosesionComponent } from './modules/autentificacion/pages/iniciosesion/iniciosesion.component';
+import { RegistroComponent } from './modules/autentificacion/pages/registro/registro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciosesionComponent,
+    RegistroComponent,
+
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
