673423c57dbf0e01ea2eb82b6aeea7a73f32c471
SARAIFIGUEROA <Saraifigueroa012@gmail.com>
2024-07-22 01:02:26 -0300

    footer

--- src/app/modules/productos/productos-routing.module.ts
+++ src/app/modules/productos/productos-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PantalonesComponent} from './pages/pantalones/pantalones.component'
+import { PantalonesComponent } from './pages/pantalones/pantalones.component'
 import { CamperasComponent } from './pages/camperas/camperas.component'
 import { GuantesComponent } from './pages/guantes/guantes.component';
 import { GorrosComponent } from './pages/gorros/gorros.component';
@@ -8,18 +8,21 @@ import { BotasComponent } from './pages/botas/botas.component';
 import { AntiparrasComponent } from './pages/antiparras/antiparras.component';
 //rutas para las categorias
 const routes: Routes = [
+
   {
-    path:"pantalones", component:PantalonesComponent
-  },
+    path: "camperas", component: CamperasComponent
+  }, 
   {
-    path:"camperas", component:CamperasComponent
-  },
+    path: "pantalones", component: PantalonesComponent
+  }, 
+  { path: "antiparras", component: AntiparrasComponent },
   {
-    path:"guantes", component:GuantesComponent
+    path: "guantes", component: GuantesComponent
   },
-  {path:"gorros", component:GorrosComponent},
-  {path:"botas", component:BotasComponent},
-  {path:"antiparras", component:AntiparrasComponent},
+  { path: "gorros", component: GorrosComponent },
+
+  { path: "botas", component: BotasComponent },
+
 ];
 
 
